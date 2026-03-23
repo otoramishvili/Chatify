b161923d1d39a59ed2fec3ea1ae32e92d239106a
otoramishvili <otoramishvili1@gmail.com>
2026-03-24 00:20:56 +0400

    Fix backend nodemailer issue

--- backend/src/lib/nodemailer.js
+++ backend/src/lib/nodemailer.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
     port: 465,
     secure: true,
     auth: {
