08ea00a3024ccf2da57fd9bf53137a11a64d14c9
otoramishvili <otoramishvili1@gmail.com>
2026-04-25 01:00:07 +0400

    Working on video calling

--- backend/dist/routes/auth.route.js
+++ backend/dist/routes/auth.route.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {} from "express";
 import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { authorize, protectRoute } from "../middleware/auth.middleware.js";
 import { rateLimitMiddleware } from "../middleware/rate.limit.middleware.js";
@@ -8,6 +8,8 @@ router.post("/login", login);
 router.post("/logout", logout);
 router.put("/update-profile", protectRoute, rateLimitMiddleware, updateProfile);
 router.get("/check", protectRoute, rateLimitMiddleware, checkAuth);
-router.get("/admin", protectRoute, authorize("admin"));
+router.get("/admin", protectRoute, authorize("admin"), (req, res) => {
+    res.status(200).json(req.user);
+});
 export default router;
 //# sourceMappingURL=auth.route.js.map
